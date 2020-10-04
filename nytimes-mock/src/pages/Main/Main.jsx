2e54d6b678148a5ee6e396823fee764db3b7a70f
Lineng Cao <linengca@usc.edu>
2020-10-04 01:08:31 -0700

    Update: fix nytimes max width

--- nytimes-mock/src/pages/Main/Main.jsx
+++ nytimes-mock/src/pages/Main/Main.jsx
@@ -30,18 +30,18 @@ const Main = () => {
 
   return (
     <div className="relative w-full min-h-screen" onClick={handleClickOutside}>
-      <div className="max-w-7xl m-auto md:px-6 z-0">
+      <div className="max-w-6xl m-auto md:px-6 z-0">
         <HeaderNav ref={myMenuButton} handleMenuOnClick={handleClickInside} />
       </div>
 
-      <div className="max-w-7xl m-auto md:px-6 z-0">
+      <div className="max-w-6xl m-auto md:px-6 z-0">
         <SpotlightSection
           spotLightNews={spotLightNews}
           news={news.slice(0, 3)}
         />
       </div>
 
-      <div className="flex md grid sm:grid-flow-col sm:grid-cols-3">
+      <div className="max-w-6xl m-auto md grid sm:grid-flow-col sm:grid-cols-3">
         <div className="col-span-2">
           <TopStoriesSection
             topStories={news.slice(3, 6)}
